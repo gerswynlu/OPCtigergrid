d2a5ed96160d89fdf71dd0838f90d617b347cf18
gerswynlu <142557228+gerswynlu@users.noreply.github.com>
2024-05-27 10:03:56 +0800

    Delete TigerGridJava-master directory

--- TigerGridJava-master/tigergrid/src/main/java/com/example/tigergrid/repository/FormRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tigergrid.repository;
-
-import com.example.tigergrid.model.Form.Form;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormRepository extends JpaRepository<Form, Integer> {
-}
--- TigerGridJava-master/tigergrid/src/main/java/com/example/tigergrid/repository/LoggedInRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tigergrid.repository;
-
-import com.example.tigergrid.model.LoggedIn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoggedInRepository extends JpaRepository<LoggedIn, Integer> {
-}
